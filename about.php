4f1c1f592d37def6453fcc52d20885d5dfe7ae2c
Teemu Puukko <teemuki@gmail.com>
2014-02-08 19:37:01 +0200

    Github link

--- about.php
+++ about.php
@@ -9,6 +9,8 @@
 		<p>Tahmatassujen reseptikirja on tarkoitettu jokapäiväiseen koti käyttöön. Kaiken ikäisille ja kaiken karvaisille heeboille.</p>
 		<p>Kaikki reseptit jotka sivulta löytyvät on testattu ja hyväksi todettuja. Mitään reseptiä ei sivustolle lisätä ellei sitä ole meidän toimesta leivottu, paistettu tai kokattu. Ja tietysti maistettu.</p>
 		<p><i>Tahmatassus</i></p>
+
+		<a href="https://github.com/puumuki/tahmatassujen-reseptit">Githubissa</a>
 	</div>
 </div> <!-- #main-container -->
 
